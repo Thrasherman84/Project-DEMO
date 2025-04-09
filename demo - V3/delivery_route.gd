a53fdcd08d15bff2f535dc1167d4fdc22752afcd
IuriiTerentev <terentievyurii@yandex.ru>
2025-04-09 23:52:07 +1000

    delivery_route scene added
    delivery_test scene shows how delivery_route is meant to be used. Emits a delivery_finished signal once the delivery is finished. First godot script ever so any criticism is welcome.

---
+++ demo - V3/delivery_route.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+signal delivery_finished
+
+@export
+var player : RigidBody3D
+
+@export
+var path_checkpoints : Array[Area3D]
+var next_checkpoint
+var current_checkpoint_id = 0
+
+@export
+var debug_mode = false
+@export
+var debug_material: StandardMaterial3D
+
+func _ready() -> void:
+	path_checkpoints[current_checkpoint_id].body_entered.connect(checkpointReached.bind())
+	
+	if(debug_mode == true):
+		for checkpoint in path_checkpoints:
+			var debug_mesh = MeshInstance3D.new()
+			
+			debug_mesh.mesh = BoxMesh.new()
+			
+			debug_mesh.set_surface_override_material(0, debug_material)
+			
+			checkpoint.add_child(debug_mesh)
+
+func checkpointReached(bodyEntered: Node3D):
+	if(bodyEntered == player):
+		path_checkpoints[current_checkpoint_id].body_entered.disconnect(checkpointReached.bind())
+		selectNextCheckpoint()
+
+func selectNextCheckpoint():
+	current_checkpoint_id += 1
+	if(current_checkpoint_id < path_checkpoints.size()):
+		next_checkpoint = path_checkpoints[current_checkpoint_id]
+		path_checkpoints[current_checkpoint_id].body_entered.connect(checkpointReached.bind())
+	else:
+		delivery_finished.emit()
+		print("delivery finished")
